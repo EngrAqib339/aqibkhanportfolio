cd108b0abea77cbe3d13c8c24d236a7c536c9a19
EngrAqib339 <17mdswe0667@uetmardan.edu.pk>
2021-03-27 18:44:52 +0000

    Second-commit

--- pages/certification.vue
+++ pages/certification.vue
@@ -27,7 +27,7 @@
       </div>
       <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
         <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" :src="require('/workspace/nuxt-website/assets/images/content-writing.png')">
+          <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" :src="require('/assets/images/CWrt.jpg')">
           <div class="flex-grow">
                         <p class="text-gray-500">Content Writing</p>
           </div>
